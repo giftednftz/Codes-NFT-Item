7129a2b845a10f53af940bba91c25d6163696cd8
Gifted <113503356+giftednftz@users.noreply.github.com>
2023-04-05 16:43:25 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Article from './Article';
+
+function App() {
+  return (
+    <div>
+      <Article />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Article.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Article() {
+  return (
+    <div>
+      <h1>NFT Shill Art - EP #1</h1>
+      
+      <h3>Preface by Doge Destiny, Founder of NFT Item Land Shill art is a new term for describing an upcoming phenomenon in the modern art space. Since the inception of NFTs in our shared reality, art has evolved in ways impossible to conceive even just a couple of years prior. The abstract pathways unearthed by this new token technology has driven the market to produce a more drastic, stylistic, and emotionally charged version of what we know as art. Without further delay, here's Gifted, art connoisseur, and his take on what Shill Art is.</h3>
+      
+      <h3>1. Till Death Do Us </h3>
+      <p>Part An interesting 1/1 art piece by amazing artist, Khadee Ize. All her arts depicts strong feelings and I really like her choice of colors and art concept. @Khadee_ize on Twitter. Great artist from Nigeria.</p>
+      
+      <h3>2. IN MY FEELINGS</h3>
+      <p>An interesting abstract piece by Dreambrand, minted on manifold with high resolution 8k. Colorful and outstanding among other abstract artists. You'll definitely want to check out all he has going on with the abstracts and Dream patterns on OS. Twitter handle @Dream_NFT1. Another great Nigerian artist.</p>
+
+      <img src="https://i.seadn.io/gae/tV8LTB8DQDVtqOgK43y8nHWRGaTntocybPZYrSL9DJRKzR92FAwWzydWa-yWCVJJA8PdaodV2FcoFys6DshsHHHcAzpwVApDx8joXg?auto=format&w=1000" alt="Gifted PFP" style={{ width: '300px', height: '300px' }} />
+    </div>
+  );
+}
+
+export default Article;
